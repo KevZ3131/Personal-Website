cb04025e2c50ab018c8bbc72523f27e3d39ad771
KevZ3131 <117871987+KevZ3131@users.noreply.github.com>
2024-12-31 23:07:54 -0500

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        faustina: ['Faustina', 'serif'],
+        alfa: ["Alfa Slab One", 'serif']
+      },
+      colors: {
+        warmBeige: '#F5E9DA',
+        softCreamWhite: '#FFFDF8',
+        navyBlue: '#1D3C5D',
+        lightCyan: '#4169E1'
+      }
+    },
   },
   plugins: [],
 }
