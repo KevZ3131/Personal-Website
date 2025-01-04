7bead004171d8cdd17d6b39a6adaf9342a3482fb
KevZ3131 <117871987+KevZ3131@users.noreply.github.com>
2025-01-03 20:07:21 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,31 @@ module.exports = {
         navyBlue: '#1D3C5D',
         lightCyan: '#4169E1',
         midnightBlue: '#191970'
+      },
+      animation: {
+        fadeIn: "fadeIn 1s ease-out",
+        fadeInDelay: "fadeIn 2s ease-out",
+        slideInLeft: "slideInLeft 1s ease-out",
+        slideInRight: "slideInRight 1s ease-out",
+        bounceOnHover: "bounce 0.5s infinite",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        slideInLeft: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        slideInRight: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        bounce: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-10px)" },
+        }
       }
     },
   },
